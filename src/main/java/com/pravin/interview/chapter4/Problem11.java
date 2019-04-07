5309b6e065930b48ff7190c767b070ed9e455f8d
Pravin <pravin.patel4592@gmail.com>
2019-04-06 22:36:54 -0400

    Adding new problem11!!

--- src/main/java/com/pravin/interview/chapter4/Problem11.java
+++ src/main/java/com/pravin/interview/chapter4/Problem11.java
@@ -1,6 +1,18 @@
 package com.pravin.interview.chapter4;
 
 public class Problem11 {
+    public static void main(String[] args) {
+        TreeNodeWithSize tree = new TreeNodeWithSize(10);
+        tree.insert(10);
+        tree.insert(12);
+        tree.insert(13);
+        tree.insert(14);
+        tree.insert(16);
+        tree.insert(1);
+        tree.insert(9);
 
+        tree.print();
+        System.out.println( tree.getRandomNode());
+    }
 
 }
--- src/main/java/com/pravin/interview/chapter4/TreeNodeWithSize.java
+++ src/main/java/com/pravin/interview/chapter4/TreeNodeWithSize.java
@@ -70,18 +70,6 @@ public class TreeNodeWithSize {
         }
     }
 
-    public static void main(String[] args) {
-        TreeNodeWithSize tree = new TreeNodeWithSize(10);
-        tree.insert(10);
-        tree.insert(12);
-        tree.insert(13);
-        tree.insert(14);
-        tree.insert(16);
-        tree.insert(1);
-        tree.insert(9);
 
-        tree.print();
-        System.out.println( tree.getRandomNode());
-    }
 
 }
