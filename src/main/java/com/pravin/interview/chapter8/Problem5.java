732bd3d0387a96c1fd91d5e1392ea51614530d76
Pravin <pravin.patel4592@gmail.com>
2019-04-07 23:59:16 -0400

    Chapter 8 problem 5

---
+++ src/main/java/com/pravin/interview/chapter8/Problem5.java
@@ -0,0 +1,12 @@
+package com.pravin.interview.chapter8;
+
+public class Problem5 {
+
+    public static void main(String[] args) {
+        Multiply multiply = new Multiply();
+        System.out.println(multiply.multiply(5, 5));
+
+        System.out.println(multiply.multiply(115, 100));
+
+    }
+}
