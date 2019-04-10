fe9c05c6c67ee46b8f14c02cb8c1394c759e8820
Pravin <pravin.patel4592@gmail.com>
2019-04-09 23:35:51 -0400

    Chapter 8 problem 7

---
+++ src/main/java/com/pravin/interview/chapter8/Problem7.java
@@ -0,0 +1,22 @@
+package com.pravin.interview.chapter8;
+
+import java.util.List;
+
+public class Problem7 {
+
+    public static void main(String[] args) {
+
+        permutationsWithoutDups permutaionWithoutDups = new permutationsWithoutDups();
+        List<String> s = permutaionWithoutDups.permutations1("abc");
+        System.out.println(s);
+
+        s = permutaionWithoutDups.permutations2("abc");
+        System.out.println(s);
+
+
+        s = permutaionWithoutDups.permutations3("abc");
+        System.out.println(s);
+
+
+    }
+}
