49941421a9a307169c8005b060581ecf3f6634d4
Pravin <pravin.patel4592@gmail.com>
2019-04-15 00:05:23 -0400

    ch10

---
+++ src/main/java/com/pravin/interview/chapter10/SortedMerge.java
@@ -0,0 +1,24 @@
+package com.pravin.interview.chapter10;
+
+public class SortedMerge {
+
+    public void sortAndMerge(int[] a, int[] b, int lastA, int lastB) {
+        int idxA = lastA -1;
+        int idxB = lastB -1;
+        int mergedIdx = lastA + lastB -1;
+
+        while(idxB >= 0){
+
+            if (idxA >= 0 && a[idxA] > b[idxB]) {
+                a[mergedIdx] = a[idxA];
+                idxA--;
+            }else {
+                a[mergedIdx] = b[idxB];
+                idxB--;
+            }
+
+            mergedIdx--;
+        }
+
+    }
+}
---
+++ src/main/java/com/pravin/interview/sorts/QuickSort.java
@@ -0,0 +1,61 @@
+package com.pravin.interview.sorts;
+
+import java.util.Arrays;
+
+public class QuickSort {
+
+    public void quickSort(int [] arr){
+        quickSort(arr, 0, arr.length-1);
+    }
+
+    private void quickSort(int[] arr, int left, int right) {
+
+        if (left >= right) {
+            return;
+        }
+        int pivot = arr[(left + right)/2];
+        int index = partition(arr, left, right, pivot);
+        quickSort(arr, left, index - 1 );
+        quickSort(arr, index , right);
+    }
+
+    private int partition(int[] arr, int left, int right, int pivot) {
+
+        while (left <= right) {
+            while(arr[left] <  pivot){
+                left++;
+            }
+
+            while (arr[right] > pivot) {
+                right--;
+            }
+
+            if (left <= right) {
+                swap(arr, left, right);
+                left++;
+                right --;
+            }
+        }
+
+        return left;
+    }
+
+    private void swap(int[] arr, int left, int right) {
+        int tmp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = tmp;
+
+    }
+
+    public static void main(String[] args) {
+        QuickSort quickSort = new QuickSort();
+        int[] a = new int[]{1, 10, 9, 2, 4};
+        Arrays.stream(a).forEach(aa -> System.out.print(" "+aa));
+        System.out.println();
+        quickSort.quickSort(a);
+
+
+        Arrays.stream(a).forEach(aa -> System.out.print(" "+aa));
+    }
+
+}
