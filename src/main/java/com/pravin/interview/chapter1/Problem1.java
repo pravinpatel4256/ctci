b77f16ed1e45ed6275e0803f2ca2ad7a901f6bd3
Pravin <pravin.patel4592@gmail.com>
2019-04-19 18:58:19 -0400

    Chapter 1

---
+++ src/main/java/com/pravin/interview/chapter1/Problem1.java
@@ -0,0 +1,14 @@
+package com.pravin.interview.chapter1;
+
+public class Problem1 {
+
+    public static void main(String[] args) {
+        UniqueString uniqueString = new UniqueString();
+        System.out.println( uniqueString.isUniqueChars("Hello how are you"));
+        System.out.println( uniqueString.isUniqueChars("ABC"));
+
+        System.out.println( uniqueString.isUniqueChars("Pravin"));
+        System.out.println( uniqueString.isUniqueChars("book"));
+
+    }
+}
