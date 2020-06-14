f8ff96714aece0793792964598781b721caa3fb6
bakky <neelammpatel35@gmail.com>
2020-06-13 22:34:30 -0700

    Check if given LinkedList isPalindrome

---
+++ src/main/java/com/pravin/interview/ctci/chapter2/Palindrome.java
@@ -0,0 +1,11 @@
+package com.pravin.interview.ctci.chapter2;
+
+import com.pravin.interview.ctci.linkedlist.Node;
+
+public class Palindrome {
+
+    public boolean isPalindrome(Node node){
+
+        return false;
+    }
+}
