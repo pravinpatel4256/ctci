4ac8b28ef8a4974c5e3029c485647b31df336619
Pravin <pravin.patel4592@gmail.com>
2020-07-06 20:34:41 -0700

    Tree and Graph

--- src/main/java/com/pravin/interview/ctci/chapter4/phase2/CheckedBalanceTree.java
+++ src/main/java/com/pravin/interview/ctci/chapter4/phase2/CheckedBalanceTree.java
@@ -34,11 +34,13 @@ public class CheckedBalanceTree {
         }
 
         int leftHeight = checkHeight(root.left);
+        System.out.println(String.format("leftHeight = %d of node: %d ", leftHeight, root.data));
         if (leftHeight == Integer.MIN_VALUE) {
             return Integer.MIN_VALUE;
         }
 
         int rightHeight = checkHeight(root.right);
+        System.out.println(String.format("rightHeight = %d of node: %d ", rightHeight, root.data));
         if (rightHeight == Integer.MIN_VALUE) {
             return Integer.MIN_VALUE;
         }
